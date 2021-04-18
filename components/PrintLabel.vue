a38698b8cf04912db5c6d26d46a4834c7796347a
Nakashoya <nakashoya@hidetaro.com>
2021-04-18 21:02:03 +0900

    labels

---
+++ components/PrintLabel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="label">
+    <div class="price-row">
+      <span class="price">{{ commodity.price }}</span>
+      円
+    </div>
+    <div>
+      <div class="barcode-wrapper">
+        <svg
+          class="barcode img-fluid"
+          jsbarcode-format="EAN13"
+          :jsbarcode-value="commodity.barcode" />
+      </div>
+    </div>
+    <div>
+      {{ commodity.name }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import JsBarcode from 'jsbarcode'
+
+@Component
+export default class PrintLabel extends Vue {
+  @Prop({ default: {} })
+  commodity!: any
+
+  mounted(): void {
+    JsBarcode('.barcode').init()
+  }
+}
+
+</script>
+
+<style scoped>
+.label {
+  background-color: white;
+  border: solid 1px;
+}
+
+.price-row {
+  background-color: yellow;
+  border: solid 1px red;
+}
+.price {
+  font-size: 2rem;
+}
+
+.barcode-wrapper {
+  width: 3cm;
+}
+</style>
---
+++ pages/labels.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="all">
+    <div class="labels">
+      <PrintLabel
+        v-for="commodity in commodities"
+        :commodity="commodity"
+        :key="commodity.barcode" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import commodityList from '../static/commodities.json'
+
+@Component
+export default class Labels extends Vue {
+  get commodities(): any[] {
+    return commodityList
+  }
+}
+
+</script>
+
+<style scoped>
+.all {
+  width: 210mm;
+}
+.labels {
+  display: flex;
+  width: 100%;
+}
+@media screen {
+  .all {
+      background: #eee;
+  }
+}
+</style>
